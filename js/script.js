a2e1080c991ac796ff6bd9f678f23d1600b7645d
mostakshaikh50 <mostak.50.cse@gmail.com>
2022-08-23 18:46:22 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -18,19 +18,44 @@ function BindPlayerName(ListOfPlayer){
             tr.innerHTML =`<th>${i+1}</th><td>${name}</td>`;
 
            tableBodyField.appendChild(tr);
+           if(name == 'Lionel-Messi'){
+            document.getElementById("mDisable").disabled = true;
+           }
+           if(name=='Kylian Mbappe'){
+            document.getElementById("kDisable").disabled = true;
+           }
+           if(name=='Cristiano Ronaldo'){
+            document.getElementById("rDisable").disabled = true;
+           }
+           if(name=='Serzio Aguero'){
+            document.getElementById("aDisable").disabled = true;
+           }
+           if(name=='Neymar'){
+            document.getElementById("nDisable").disabled = true;
+           }
+           if(name=='De Maria'){
+            document.getElementById("dDisable").disabled = true;
+           }
+           
+        }
+        else{
+            return;
         }
         
     }
 }
 function AddPlayerToList(elementFromClick){
+    
    
    const playerName = elementFromClick.parentNode.parentNode.children[0].innerText;
+   
    const PlayerObj = {
     playerName: playerName    
    }
    
    ListOfPlayer.push(PlayerObj);   
    BindPlayerName(ListOfPlayer);
+   
 }
 
 
@@ -41,8 +66,11 @@ function CalculatePerPlayerAmount(){
         alert("Don't Have Player in the list");
     }
     else{
-        const playerExpenses = ListOfPlayer.length * perPlayerValue;
-        document.getElementById("expensesMoney").innerText = parseFloat(playerExpenses);
+        
+            const playerExpenses = ListOfPlayer.length * perPlayerValue;
+            document.getElementById("expensesMoney").innerText = parseFloat(playerExpenses);
+      
+        
     }
 }
 
